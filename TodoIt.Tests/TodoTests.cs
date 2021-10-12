8da7897b1ff6e983d35e636c6adc31e7f5adebfa
Ahmad Zarour <zarour.ahmad@gmail.com>
2021-10-12 22:15:54 +0200

    Todo class created and Unit Test for the constructor

---
+++ TodoIt.Tests/TodoTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using TodoIt.Model;
+using Xunit;
+
+namespace TodoIt.Tests
+{
+    public class TodoTests
+    {
+
+        ///Testing the constructor of the class Todo by create an instance of it and check values 
+        [Fact]
+        public void Constructor_ShouldCreate_AnInstanceOf_TodoClass()
+        {
+            int todoId = 101;
+            string description = "Descriptions about the task";
+            Todo sut = new Todo(todoId, description);
+            Assert.Equal(todoId, sut.TodoId);
+            Assert.Equal(description, sut.Description);
+
+        }
+    }
+}
---
+++ TodoIt/Model/Todo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TodoIt.Model
+{
+    public class Todo
+    {
+        // Fields , private by defult
+        readonly int _todoId;
+        string _description;
+        bool _done;
+        Person _assignee;
+
+
+
+        // Readonly property to get the TodoID
+        public int TodoId
+        {
+            get { return _todoId; }
+        }
+        public string Description
+        {
+            get { return _description; }
+            set { _description = value; }
+        }
+        public bool Done
+        {
+            get { return _done; }
+            set { _done = value; }
+        }
+        public Person Assignee
+        {
+            get { return _assignee; }
+            set { _assignee = value; }
+        }
+
+        // Constructor to initiate the Todo object, takes todoId and description
+        public Todo(int todoId, string description)
+        {
+            _todoId = todoId;
+            Description = description;
+        }
+    }
+}
