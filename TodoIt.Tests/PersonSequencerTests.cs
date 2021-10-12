49587094ad544d1b23c74194b29894c4f5d4037d
Ahmad Zarour <zarour.ahmad@gmail.com>
2021-10-12 22:58:01 +0200

    Create a class PersonSequencer and test its method nextPersonId and reset.

---
+++ TodoIt.Tests/PersonSequencerTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using TodoIt.Data;
+using Xunit;
+
+namespace TodoIt.Tests
+{
+    public class PersonSequencerTests
+    {
+        [Fact]
+        public void nextPersonIdMethod_ShouldIncrese_personId_ByOne()
+        {
+            int test1Result = PersonSequencer.nextPersonId();
+            int test2Result = PersonSequencer.nextPersonId();
+            int test3Result = PersonSequencer.nextPersonId();
+            Assert.Equal(1, test1Result);
+            Assert.Equal(2, test2Result);
+            Assert.Equal(3, test3Result);
+        }
+
+        [Fact]
+        public void resetMethod_ShouldSet_personId_ToZero()
+        {
+            PersonSequencer sut = new PersonSequencer();
+            PersonSequencer.nextPersonId();
+            PersonSequencer.nextPersonId();
+            int testResult = PersonSequencer.nextPersonId();
+            PersonSequencer.reset();
+            Assert.Equal(0,sut.PersonId);
+        }
+    }
+}
---
+++ TodoIt/Data/PersonSequencer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TodoIt.Data
+{
+    public class PersonSequencer
+    {
+        static int personId;
+
+        // Property to get the person Id value , for testing purpose
+        public int PersonId{ get { return personId; } }
+
+        // Method to increase the personId by one
+        public static int nextPersonId()
+        {
+            return ++personId;
+        }
+
+        //To reset the personId to zero
+        public static void reset()
+        {
+            personId = 0;
+        }
+    }
+}
